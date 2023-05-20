533ecc34a65d11a5f6c391872a083ad5b0c4584f
Okakakai <inoue.kai.work@gmail.com>
2023-05-20 15:23:55 +0900

    add own context

--- lib/screen/sign_in_screen.dart
+++ lib/screen/sign_in_screen.dart
@@ -2,6 +2,11 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:tutorial_samplea_application/screen/photo_list_screen.dart';
 
+import 'package:logger/logger.dart';
+
+// デフォルトでリリースビルドは出力しない、デバッグビルドはすべてログ出力する動作になっている
+final logger = Logger();
+
 class SignInScreen extends StatefulWidget {
   @override
   _SignInScreenState createState() => _SignInScreenState();
@@ -11,86 +16,136 @@ class _SignInScreenState extends State<SignInScreen> {
   // Form の key を指定する場合は、<FormState> として、 GlobalKey<FormState> を指定する
   final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
   // メールアドレス用のTextEditingController
-  final TextEditingController _emailController = TextEditingController();
+  late TextEditingController _emailController = TextEditingController();
   // パスワード用のTextEditingController
-  final TextEditingController _passwordController = TextEditingController();
+  late TextEditingController _passwordController = TextEditingController();
+
+  @override
+  void initState() {
+    super.initState();
+
+    // 変数を初期化する
+    //   - Widgetが作成された初回のみ動作させたい処理はinitState()に記述する
+    _emailController = TextEditingController();
+    _passwordController = TextEditingController();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: Center(
-      child: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 24),
-          // Columnを使い縦に並べる
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              // テキストを表示
-              Text(
-                'Photo App',
-                style: Theme.of(context).textTheme.headlineSmall,
-              ),
+        body: Form(
+            key: _formKey, // Formのkeyに指定する
+            child: Center(
+              child: Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 24),
+                  // Columnを使い縦に並べる
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      // テキストを表示
+                      Text(
+                        'Photo App',
+                        style: Theme.of(context).textTheme.headlineSmall,
+                      ),
 
-              const SizedBox(height: 16),
+                      const SizedBox(height: 16),
 
-              TextFormField(
-                // TextEditingController,
-                controller: _emailController,
-                decoration: const InputDecoration(labelText: "メールアドレス"),
-                keyboardType: TextInputType.emailAddress,
-                validator: (String? value) {
-                  if (value?.isEmpty == true) {
-                    return 'メールアドレスを入力してください';
-                  }
-                  return null;
-                },
-              ),
+                      TextFormField(
+                        // TextEditingController,
+                        controller: _emailController,
+                        decoration: const InputDecoration(labelText: "メールアドレス"),
+                        keyboardType: TextInputType.emailAddress,
+                        validator: (String? value) {
+                          if (value?.isEmpty == true) {
+                            ScaffoldMessenger.of(context)
+                                .showSnackBar(const SnackBar(
+                              content: Text('メールアドレスを入力してください'),
+                              backgroundColor: Colors.red,
+                            ));
+                            logger.d('メールアドレスを入力してください');
+                            return 'メールアドレスを入力してください';
+                          }
+                          return null;
+                        },
+                      ),
 
-              const SizedBox(height: 8),
+                      const SizedBox(height: 8),
 
-              TextFormField(
-                // TextEditingControllerを設定
-                controller: _passwordController,
-                decoration: const InputDecoration(labelText: "パスワード"),
-                keyboardType: TextInputType.visiblePassword,
-                obscureText: true,
-                validator: (String? value) {
-                  if (value?.isEmpty == true) {
-                    return 'パスワードを入力してください';
-                  }
-                  return null;
-                },
-              ),
+                      TextFormField(
+                        // TextEditingControllerを設定
+                        controller: _passwordController,
+                        decoration: const InputDecoration(labelText: "パスワード"),
+                        keyboardType: TextInputType.visiblePassword,
+                        obscureText: true,
+                        validator: (String? value) {
+                          if (value?.isEmpty == true) {
+                            ScaffoldMessenger.of(context)
+                                .showSnackBar(const SnackBar(
+                              content: Text('パスワードを入力してください'),
+                              backgroundColor: Colors.red,
+                            ));
+                            logger.d('パスワードを入力してください');
+                            return 'パスワードを入力してください';
+                          }
+                          return null;
+                        },
+                      ),
 
-              const SizedBox(height: 16),
+                      const SizedBox(height: 16),
 
-              SizedBox(
-                width: double.infinity,
-                child: ElevatedButton(
-                  // ログインボタンをタップしたときの処理
-                  onPressed: () => _onSignIn(),
-                  child: const Text('log in'),
-                ),
-              ),
+                      Container(
+                        width: double.infinity,
+                        child: ElevatedButton(
+                          // ログインボタンをタップしたときの処理
+                          onPressed: () => _onSignIn(context),
+                          child: const Text('ログイン'),
+                        ),
+                      ),
 
-              const SizedBox(height: 8),
-
-              SizedBox(
-                width: double.infinity,
-                //Button
-                child: ElevatedButton(
-                  // 新規登録ボタンをタップしたときの処理
-                  onPressed: () => _onSignUp(),
-                  child: const Text('Sign up'),
-                ),
-              ),
-            ],
-          )),
-    ));
+                      const SizedBox(height: 16),
+                      Container(
+                        width: double.infinity,
+                        //Button
+                        child: ElevatedButton(
+                          // 新規登録ボタンをタップしたときの処理
+                          onPressed: () => _onSignUp(context),
+                          child: const Text('新規登録'),
+                        ),
+                      ),
+                    ],
+                  )),
+            )));
   }
 
-  Future<void> _onSignIn() async {
-    try {} catch (e) {
+  Future<void> _onSignIn(BuildContext context) async {
+    try {
+      // Form の内容をチェックする
+      if (_formKey.currentState?.validate() != true) {
+        // If the form is valid, display a snackbar. In the real world,
+        // you'd often call a server or save the information in a database.
+        ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
+          content: Text('入力内容に足りない項目があります'),
+          backgroundColor: Colors.red,
+        ));
+        logger.d(
+            'onSign \n _formKey.currentState?.validate()  \n エラーメッセージがあるため処理を中断する');
+        return;
+      }
+
+      final String email = _emailController.text;
+      final String password = _passwordController.text;
+      await FirebaseAuth.instance.signInWithEmailAndPassword(
+        email: email,
+        password: password,
+      );
+
+      //画像一覧画面に遷移する
+      Navigator.of(context).pushReplacement(
+        MaterialPageRoute(
+          builder: (_) => PhotoListScreen(),
+        ),
+      );
+    } catch (e) {
       await showDialog(
           context: context,
           builder: (context) {
@@ -100,34 +155,23 @@ class _SignInScreenState extends State<SignInScreen> {
             );
           });
     }
-    // Form の内容をチェックする
-    if (_formKey.currentState?.validate() != true) {
-      // エラーメッセージがあるため処理を中断する
-      return;
-    }
-
-    final String email = _emailController.text;
-    final String password = _passwordController.text;
-    await FirebaseAuth.instance.signInWithEmailAndPassword(
-      email: email,
-      password: password,
-    );
-
-    //画像一覧画面に遷移する
-    Navigator.of(context).pushReplacement(
-      MaterialPageRoute(
-        builder: (_) => PhotoListScreen(),
-      ),
-    );
   }
 
   // 内部で非同期処理(Future)を扱っているのでasyncを付ける
   //   この関数自体も非同期処理となるので返り値もFutureとする
-  Future<void> _onSignUp() async {
+  Future<void> _onSignUp(BuildContext context) async {
     try {
       // Form の内容をチェックする
       if (_formKey.currentState?.validate() != true) {
+        // If the form is valid, display a snackbar. In the real world,
+        // you'd often call a server or save the information in a database.
+        ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
+          content: Text('入力内容に足りない項目があります'),
+          backgroundColor: Colors.red,
+        ));
         // エラーメッセージがあるため処理を中断する
+        logger.d(
+            'onSign \n formKey.currentState?.validate() \n エラーメッセージがあるため処理を中断する');
         return;
       }
 
