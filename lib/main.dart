879278cd20e40b06753b14d9811e42bf5b833515
Okakakai <inoue.kai.work@gmail.com>
2023-05-20 13:55:12 +0900

    fixed package error

--- lib/main.dart
+++ lib/main.dart
@@ -7,8 +7,15 @@ import 'package:tutorial_samplea_application/screen/providers.dart';
 import 'package:tutorial_samplea_application/screen/sign_in_screen.dart';
 
 void main() async {
+  // Flutterの初期化処理を待つ
   WidgetsFlutterBinding.ensureInitialized();
+
+  // アプリ起動前にFirebase初期化処理を入れる
+  //   - initializeApp()の返り値がFutureなので非同期処理
+  //   - 非同期処理(Future)はawaitで処理が終わるのを待つことができる
+  //   - ただし、awaitを使うときは関数にasyncを付ける必要がある
   await Firebase.initializeApp();
+
   runApp(
     // Providerで定義したデータを渡せるようにする
     ProviderScope(
